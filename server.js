659b6d18835f56d41a522d1275f51bb57f5afffa
Shubhra-js <shubhrajyotsna06@gmail.com>
2026-06-19 15:22:31 +0530

    Initial Express server setup

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from "express";
-
+import dotenv from "dotenv";
+dotenv.config();
 const app = express();
 
 app.get("/", (req, res) => {
@@ -8,4 +9,7 @@ app.get("/", (req, res) => {
 
 app.listen(3000, () => {
     console.log("Server running on port 3000");
+});
+app.get("/hello", (req, res) => {
+  res.send("Hello from DSA Instructor!");
 });
